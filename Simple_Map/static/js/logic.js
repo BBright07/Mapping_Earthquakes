d9e85fd314b366f0d525eaf4267d08601f83c214
BBright07 <89029747+BBright07@users.noreply.github.com>
2022-01-05 15:55:23 -0500

    bleu line

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -26,8 +26,7 @@ let line = [ [37.615223,	-122.389977],
 //];
   // Create a polyline using the line coordinates and make the line red.
 L.polyline(line, {
-  color: "blue", linewidth: 4, opacity: 0.5,
-}).addTo(map);
+  color: "blue", linewidth: 4, opacity: 0.5, }).addTo(map); 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
