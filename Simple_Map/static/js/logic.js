77957068b3f3ad87b85510ffc1aaf181bfba5800
BBright07 <89029747+BBright07@users.noreply.github.com>
2022-01-05 15:47:38 -0500

    added blue trajectory SF to NYC + 2 stops

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -7,12 +7,29 @@ console.log("working");
 // Create the map object with a center and zoom level.
 //let map = L.map('mapid').setView([40.7, -94.5], 4);
 // Create the map object with a center and zoom level.
-let map = L.map("mapid", {center: [34.0522, -118.2437],zoom: 14
+let map = L.map("mapid", {center: [37.6213, -122.3790],zoom: 5
   });
-//  Add a marker to the map for Los Angeles, California.
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//Airport stop line
+let line = [ [37.615223,	-122.389977],
+[30.18999924, -97.668663992], 
+[	43.6777171,-79.624819699],
+[40.658732,-73.812131]
+
+]
+// Coordinates for each point to be used in the polyline.
+//let line = [
+  //[33.9416, -118.4085],
+  //[37.6213, -122.3790],
+  //[40.7899, -111.9791],
+  //[47.4502, -122.3088]
+//];
+  // Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "blue", linewidth: 4, opacity: 0.5,
+}).addTo(map);
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEYX
